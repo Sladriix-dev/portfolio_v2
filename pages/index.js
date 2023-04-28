6a83639f70c0bde359380a5c1e7df4fffb2b5a6b
Sladriix <sladriix@gmail.com>
2023-04-28 12:55:55 +0200

    ADD AnimatedText component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Layout from "@/components/Layout"
 import Head from "next/head"
 import Image from "next/image"
 import profilePic from "../public/assets/profilePic.png"
+import AnimatedText from "@/components/AnimatedText";
+import Link from "next/link";
+import { LinkArrow } from "@/components/Icons";
 
 export default function Home() {
   return (
@@ -20,16 +23,31 @@ export default function Home() {
                 className="w-full h-auto"
               />
             </div>
-            <div>
-              <h1>
-                Transformer la Vision en Réalité avec le Code et le Design
-              </h1>
-              <p>
-                En tant que développeur front-end qualifié, je me consacre à
+            <div className="w-1/2 flex flex-col items-center self-center">
+              <AnimatedText text="Transformer la Vision en Réalité avec le Code et le Design" className="!text-6xl" />
+              <p className="my-4 text-base font-medium">
+                En tant que développeur front-end qualifié je me consacre à
                 transformer des idées en applications Web innovantes. Explorez
                 mes derniers projets, mettant en valeur mon
                 expertise en React.js et en développement web.
               </p>
+              <div className="flex items-center self-start mt-2">
+                <Link 
+                  href="/CV_Kévin_Forget.pdf" 
+                  target={"_blank"}
+                  className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark"
+                  download={true}
+                >
+                  Mon&nbsp;CV<LinkArrow className={"w-6 ml-1"} />
+                </Link>
+                <Link 
+                  href="mailto:kevin.forget0805@gmail.com" 
+                  target={"_blank"}
+                  className="ml-4 text-lg font-medium capitalize text-dark underline"
+                >
+                  Me contacter
+                </Link>
+              </div>
             </div>
           </div>
         </Layout>
