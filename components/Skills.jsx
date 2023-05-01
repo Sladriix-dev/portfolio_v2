0062d4c96c4be66f4bf1c00e19f1ad902550f26e
Sladriix <sladriix@gmail.com>
2023-05-01 13:01:30 +0200

    ADD about page full responsive

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -18,7 +18,7 @@ const Skill = ({ name, x, y }) => {
 const Skills = () => {
   return (
     <>
-      <h2 className="font-bold text-8xl mt-36 w-full text-center md:text-6xl md:mt-16">
+      <h2 className="font-bold mb-5 text-8xl mt-36 w-full text-center md:text-6xl md:mt-16">
         Compétences
       </h2>
       <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark lg:h-[80vh] sm:h-[50vh] xs:h-[50vh] lg:bg-circularLightLg lg:dark:bg-circularDarkLg md:bg-circularLightMd md:dark:bg-circularDarkMd sm:bg-circularLightSm sm:dark:bg-circularDarkSm">
